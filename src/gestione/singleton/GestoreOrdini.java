3125e03d2343419d862fefd796b85a83ec6f5e35
Michela <micheladellagatta1@gmail.com>
2026-04-08 16:20:41 +0200

    fix minor bugs

--- src/gestione/singleton/GestoreOrdini.java
+++ src/gestione/singleton/GestoreOrdini.java
@@ -1,76 +1,60 @@
 package gestione.singleton;
+
+import gestione.observer.Ordine;
 import gestione.decorator.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GestoreOrdini 
-{
-    
+public class GestoreOrdini {
     private static GestoreOrdini istanza;
 
-    //ordine attuale
-    private List<Pizza> ordineCorrente;
-    
-     private String statoOrdine;
-    
-     private GestoreOrdini() 
-     {
-        ordineCorrente=new ArrayList<>();
-        statoOrdine="Nessun ordine";
-        
-    }
+    // ordine attuale
+    private List<Ordine> storicoOrdini = new ArrayList<>();
+    private String statoOrdine;
 
-     public static GestoreOrdini getInstance() {
+    private GestoreOrdini() {}
+
+    public static GestoreOrdini getInstance() {
         if (istanza == null) {
             istanza = new GestoreOrdini();
         }
         return istanza;
     }
 
-
-    public void aggiungiOrdine(Pizza pizza) {
-        ordineCorrente.add(pizza);
-        System.out.println("Aggiunto: " + pizza.getDescrizione());
+    public void aggiungiOrdine(Ordine ordine) {
+        storicoOrdini.add(ordine);
     }
 
+    public List<Ordine> getOrdini() {
+        return storicoOrdini;
+    }
 
-    
-    public void modificaStato(String nuovoStato)
-    {
-        if(ordineCorrente.isEmpty())
-        {
+    /*     
+    public void modificaStato(String nuovoStato) {
+        if (storicoOrdini.isEmpty()) {
             System.out.println("Nessun ordine corrente");
-        }
-        else
-        {
+        } else {
             statoOrdine = nuovoStato;
         }
 
-    }
-
-
-    public void visualizzaOrdine()
-    {
-         if(ordineCorrente.isEmpty())
-        {
+    } 
+    
+    public void visualizzaOrdine() {
+        if (storicoOrdini.isEmpty()) {
             System.out.println("Nessun ordine corrente");
+        } else {
+            System.out.println("Ordine Corrente: ");
+        double tot = 0;
+        
+        for (Pizza p : storicoOrdini) {
+            System.out.println(" - " + p.getDescrizione() +
+            " | Prezzo: " + p.getPrezzo());
+            tot += p.getPrezzo();
         }
-        else
-        {
-             System.out.println("Ordine Corrente: ");
-            double tot=0;
-            
-            for(Pizza p: ordineCorrente)
-            {
-                System.out.println(" - " + p.getDescrizione() + 
-                                   " | Prezzo: " + p.getPrezzo());
-                tot += p.getPrezzo();
-            }
-            
-        }
-
-
+        
     }
+    
+}
+*/
 
 }
-
